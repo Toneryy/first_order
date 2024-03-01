54c4a6890d24e0bd094e45198253ca3ea9eba755
Toneryy <colorful1.off@gmail.com>
2024-03-01 19:00:01 +0300

    Начинаю адаптив

---
+++ nav.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-buttn');
+const navBtnImg = document.querySelector('#nav-button-image');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "./data/nav-close.svg";
+    } else {
+        navBtnImg.src = "./data/catalog-mb.png";
+        console.log("Пиздец")
+    }
+}
+
